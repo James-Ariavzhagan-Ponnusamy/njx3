be5c94bdd2bbc7fca65f93904c9ba97ba98048e4
James Arivazhagan <James.Arivazhagan@gmail.com>
2022-02-23 18:46:24 +0530

    upate
    update

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -4,11 +4,7 @@ import styles from "../../styles/Home.module.css"
 function Header(){
     return(
         <div className={styles.headcontainer}>
-        <div className={styles.tabinator}>
-            <label for = "tab1">Tab1</label>
-        </div>
-        
-        
+               
         </div>
     )
 }
