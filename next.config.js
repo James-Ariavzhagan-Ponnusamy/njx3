a18600da5810b7a8515b290b5cc1d77e40f15c1d
James Arivazhagan <James.Arivazhagan@gmail.com>
2022-02-23 18:58:34 +0530

    commit
    commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-//const withImages = require("next-images")
-/*
 const nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = nextConfig
-*/
-
-//module.exports = withImages()
